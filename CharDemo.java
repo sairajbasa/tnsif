fd3f780ab635a4df2b4c82dc39dc7b6726395263
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2024-03-14 20:33:23 +0530

    committed on 14-03-2024 at 20 : 33 PM

---
+++ CharDemo.java
@@ -0,0 +1,17 @@
+package practice;
+// program to work with Ascii and unicodes in java
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		char ch ='d';
+		int value=ch;
+		System.out.println("The Ascii value of character "+ch+" = "+value);
+		
+		
+		char ch1='\u03C0';
+		System.out.println("The symbol of PI = "+ch1);		
+
+	}
+
+}
